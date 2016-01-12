9dcaf14d32aac1306a62e1999dd0f615d6305189
pchinjr <pchinjr@gmail.com>
2016-01-11 22:12:00 -0500

    cleaned up components

--- app.jsx
+++ app.jsx
@@ -1,20 +1,59 @@
+var rootURL = 'https://ghostreact.firebaseio.com/';
 
-
-var App = React.createClass ({
-    getInitialState: function() {
-      return {counter: 1}
-    },
-    handleClick: function(){
-      this.setState({counter: this.state.counter + 1 });
-    },
-    render: function() {
-      return (
-        <div className="text-center">
-          <h1>World's Most Useless Button <button onClick={this.handleClick} className="btn btn-primary btn-lg">{this.state.counter}</button> </h1>
-        
+var Form = React.createClass({
+  render: function() {
+    return <form className="form-group">
+      <div className="form-group">
+        <label className="col-sm-2 control-label">Ghost Name</label>
+          <div className="col-sm-10">
+            <input
+              placeholder="Name"
+              type="text"
+              className="form-control" />
+          </div>
+      </div>
+      <div className="form-group">
+        <label className="col-sm-2 control-label">Description</label>
+          <div className="col-sm-10">
+            <input
+              placeholder="Description"
+              type="text"
+              className="form-control" />
+          </div>
+      </div>
+      <div className="form-group">
+        <label className="col-sm-2 control-label">Photo URL</label>
+          <div className="col-sm-10">
+            <input
+              placeholder="Photo URL"
+              type="text"
+              className="form-control" />
+          </div>
+      </div>
+      <div className="form-group">
+        <div className="col-sm-offset-2 col-sm-10">
+          <submit
+            className="btn btn-default"
+            type="submit">
+            Report
+          </submit>
         </div>
-      );
+      </div>
+    </form>
+  }
+});
+
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      ghosts: {},
     }
+  },
+  render: function() {
+    return <div>
+        <Form ghostsStore={this.state.ghosts} />
+      </div>
+  }
 });
     
 ReactDOM.render( <App /> , document.getElementById('content') );
